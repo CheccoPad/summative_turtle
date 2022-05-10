91f516231c80bd85b62cb38b90573de11a210288
CheccoPad <checcopadula02@gmail.com>
2022-05-10 14:44:43 +0200

    Square are now in the correct order, and the fibonacci sequence spiral is present.

--- main.py
+++ main.py
@@ -9,10 +9,38 @@
 import turtle
 
 t1 = turtle.Turtle()
-# t2 = turtle.Turtle()
+t2 = turtle.Turtle()
+
+t1.speed(1000)
+t2.speed(1000)
+
+t1.hideturtle()
+t2.hideturtle()
+
+t1.color('orange')
+t2.color('green')
 
 
-def fibonacci_scale(b):
+def dividing_by_2(x):
+    while x > 1.0:
+        x = x / 2
+    return x
+
+
+def dividing_by_3(x):
+    while x > 1.0:
+        x = x / 3
+    return x
+
+
+def dividing_by_4(x):
+    while x > 1:
+        x = x / 4
+    return x
+
+
+def fibonacci_scale():
+    b = 1
     c = 0
 
     while c <= 4000000:
@@ -20,16 +48,19 @@ def fibonacci_scale(b):
         a = b
         b = c
         c = a + b
-        print(f' fib: {c}')
 
-        while i < 4:
+        while i < 5:
+            t1.hideturtle()
             t1.forward(c * 20)
             t1.left(90)
-            print(i)
+
             i += 1
+
+        t2.circle(c * 20, 90)
+
         t1.forward(c * 20)
 
 
-fibonacci_scale(1)
+fibonacci_scale()
 
 turtle.done()
